705f87917bf6399a370a218c2fca1db9c0802fac
Kshitij Kumar <126380805+kxhitijkumar@users.noreply.github.com>
2025-02-20 14:25:40 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-from lexer import Lexer
-
+# Test Code
 source_code = '''
 def add(x, y):
     return x + y
@@ -10,5 +9,13 @@ result = add(10, 20) # Compute sum
 lexer = Lexer(source_code)
 tokens = lexer.tokenize()
 
+print("Tokens:")
 for token in tokens:
     print(token)
+
+parser = Parser(tokens)
+ast_nodes = parser.parse()
+
+print("\nAST Nodes:")
+for node in ast_nodes:
+    print(node)
